564f2cdad5b70814b00d59b19a4b6cb5ca4ab641
Tomy Carrillo <tomicarrillo04@gmail.com>
2024-05-16 20:30:36 -0300

    Creacion de la clase Materia
    creación de la clase y sus atributos.

---
+++ src/universidadgrupo18/entidades/Materia.java
@@ -0,0 +1,11 @@
+
+package universidadgrupo18.entidades;
+
+
+public class Materia {
+    private int idMateria;
+    private String nombre;
+    private int año;
+    private boolean estado;
+    
+}
