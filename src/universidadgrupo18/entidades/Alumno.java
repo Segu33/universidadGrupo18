509c4c11e82a27712c66cc531fb8af6f3a9bf71d
Candela Guadalupe Godoy <163220967+candelagodoy@users.noreply.github.com>
2024-05-16 20:09:44 -0300

    claseAlumno
    clase alumno, mas atributos

---
+++ src/universidadgrupo18/entidades/Alumno.java
@@ -0,0 +1,14 @@
+
+package universidadgrupo18.entidades;
+
+import java.util.Date;
+
+public class Alumno {
+    private int idAlumno;
+    private int dni;
+    private String apellido;
+    private String nombre;
+    private Date fechaNacimiento;
+    private boolean estado;
+    
+}
