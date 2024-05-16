84bdd1db70fb9a74b2d813545f8309a9570b1a97
jose <knich0564@gmail.com>
2024-05-16 20:18:50 -0300

    creacion clase inscripcion

---
+++ src/universidadgrupo18/entidades/Inscripcion.java
@@ -0,0 +1,13 @@
+
+package universidadgrupo18.entidades;
+
+
+public class Inscripcion {
+    
+    private int idInscripto;
+    private double nota;
+    private int idAlumno;
+    private int idMateria;
+    
+    
+}
